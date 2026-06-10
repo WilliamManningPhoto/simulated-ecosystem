6913e9d3ba4dbdf595eb45be98814ff9402f0e8c
William <Willalexman@outlook.com>
2026-06-10 13:47:41 +0100

    Rocks generation

--- V3/constants.h
+++ V3/constants.h
@@ -5,3 +5,16 @@ const int CELL = 20;
 const int MAP_HEIGHT = 800;
 const int MAP_WIDTH = 1500;
 const int GRID_SIZE = 100;
+
+// Spawn rates of entities
+const float ROCK_MIN = 0.01;
+const float ROCK_MAX = 0.05;
+
+const float HARE_MIN = 0.05;
+const float HARE_MAX = 0.15;
+
+const float FOX_MIN = 0.01;
+const float FOX_MAX = 0.03;
+
+const float TREE_MIN = 0.01;
+const float TREE_MAX = 0.02;
--- V3/environment.cpp
+++ V3/environment.cpp
@@ -1,6 +1,9 @@
 // Define the Environment constructor so its initialised
 // Definitions for all Environment functions
 
+#include <iostream>
+#include <cstdlib> 
+
 #include "environment.h"
 #include "constants.h"
 
@@ -11,25 +14,42 @@ Environment::Environment(){
             grid[y][x] = nullptr; // None in C++
         }
     }
+
+    terrain_generation();
 }
 
-
+ // Calling all placement for initial setup
 void Environment::terrain_generation(){
-
+    obstacle_placement();
+    plant_placement();
+    animal_placement();
 }
 
+ // Rock placement
 void Environment::obstacle_placement(){
+    int amount = ROCK_MIN * GRID_SIZE * GRID_SIZE + rand() % (int)((ROCK_MAX - ROCK_MIN) * GRID_SIZE * GRID_SIZE);
 
+    for (int i = 0; i < amount; i++){
+        int x = rand() % GRID_SIZE;
+        int y = rand() % GRID_SIZE;
+
+        Rock* r = new Rock(x, y);
+        rocks.push_back(r);
+        grid[y][x] = r;
+    }
 }
 
+ // Grass, Trees
 void Environment::plant_placement(){
 
 }
 
+// Hares and foxes placed
 void Environment::animal_placement(){
 
 }
 
+// If entity dies remove from game
 void Environment::entity_removal(){
 
 }
